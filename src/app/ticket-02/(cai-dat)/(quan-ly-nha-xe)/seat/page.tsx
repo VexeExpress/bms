aa66f8e43a2dcf3cf72cda100306e8c5999a8582
tuanthanh-2 <tuanthanhktltk21@gmail.com>
2024-11-09 15:00:09 +0700

    feat: seatmap

--- src/app/ticket-02/(cai-dat)/(quan-ly-nha-xe)/seat/page.tsx
+++ src/app/ticket-02/(cai-dat)/(quan-ly-nha-xe)/seat/page.tsx
@@ -1,3 +1,97 @@
+"use client"
+import ModalSeatMap from "@/modules/seat/components/ModalSeatMap";
+import { NewSeatMap } from "@/modules/seat/types/NewSeatMap";
+import { SeatMap } from "@/modules/seat/types/SeatMapData";
+import { Add } from "@mui/icons-material";
+import { Button } from "@mui/material";
+import { useState } from "react";
+
 export default function SeatPage() {
-  return <>Sơ đồ ghế</>;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalMode, setModalMode] = useState<"create" | "edit">("create");
+  const [selectedSeatMap, setSelectedSeatMap] = useState<SeatMap | null>(
+    null,
+  );
+  const handleAddSeatMapClick = () => {
+    setModalMode("create");
+    setSelectedSeatMap(null);
+    setIsModalOpen(true);
+  };
+  const handleEdit = (office: SeatMap) => {
+    setModalMode("edit");
+    setSelectedSeatMap(office);
+    setIsModalOpen(true);
+  };
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+  // const handleDelete = (officeId: number) => {
+  //   const isConfirmed = window.confirm(
+  //     "Bạn có chắc chắn muốn xóa phương tiện này?",
+  //   );
+  //   if (isConfirmed) {
+  //     deleteVehicle(officeId);
+  //   }
+  // };
+  const handleSaveSeatMap = async (
+    seatMapData: SeatMap | NewSeatMap,
+  ) => {
+    if (modalMode === "create") {
+      const newSeatMap: NewSeatMap = {
+        ...seatMapData,
+      };
+      console.log("Saving new newSeatMap:", newSeatMap);
+      await createVehicle(newSeatMap);
+    } else if (modalMode === "edit" && (seatMapData as SeatMap).id) {
+      const updatedSeatMap: SeatMap = seatMapData as SeatMap;
+      console.log("Updating updatedSeatMap:", updatedSeatMap);
+      await updateVehicle(updatedSeatMap.id, updatedSeatMap);
+    }
+    handleCloseModal();
+  };
+  return (
+    <div className="bg-white p-0">
+      <div className="mb-10 flex items-center justify-between">
+        <span className="m-0 text-[20px] font-semibold text-black">
+          Danh sách sơ đồ ghế
+        </span>
+        <Button
+          variant="contained"
+          startIcon={<Add />}
+          onClick={handleAddSeatMapClick}
+        >
+          Thêm sơ đồ ghế
+        </Button>
+      </div>
+
+      {/* {loading ? (
+      <>
+        <LoadingIndicator />
+      </>
+    ) : error ? (
+      <p>Error loading vehicle: {error}</p>
+    ) : (
+      <TableVehicle
+        data={vehicles}
+        onDelete={handleDelete}
+        onEdit={handleEdit}
+      />
+    )}
+
+    <ModalVehicle
+      open={isModalOpen}
+      onClose={handleCloseModal}
+      mode={modalMode}
+      vehicleData={selectedVehicle}
+      onSave={handleSaveVehicle}
+    /> */}
+      <ModalSeatMap
+        open={isModalOpen}
+        onClose={handleCloseModal}
+        mode={modalMode}
+        seatMapData={selectedSeatMap}
+        onSave={handleSaveSeatMap}
+      />
+    </div>
+  );
 }
---
+++ src/modules/seat/components/ModalSeatMap.tsx
@@ -0,0 +1,267 @@
+import { useEffect, useState } from "react";
+import { SeatMapData } from "../types/SeatMapData";
+import { Box, Button, Card, Grid, Grid2, MenuItem, Modal, Select, TextField, Typography } from "@mui/material";
+import { useForm } from 'react-hook-form';
+import { SeatData } from "../types/SeatData";
+import style from "../style/SeatMap.module.css";
+const styleModal = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '80%',
+    maxHeight: '80%',
+    overflowY: 'auto',
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
+interface ModalSeatMapProps {
+    open: boolean;
+    onClose: () => void;
+    onSubmit: (data: SeatMapData, initialData?: SeatMapData) => void;
+    initialData: SeatMapData | null;
+}
+
+const ModalSeatMap: React.FC<ModalSeatMapProps> = ({ open, onClose, onSubmit, initialData }) => {
+    const { register, handleSubmit, setValue, reset, formState: { errors } } = useForm<SeatMapData>({
+        mode: "onTouched",
+    });
+    const [floor, setSelectedFloor] = useState(1);
+    const [row, setSelectedRow] = useState(1);
+    const [column, setSelectedColumn] = useState(1);
+    const [seats, setSeats] = useState<SeatData[]>([]);
+    useEffect(() => {
+        if (open) {
+            console.log("Modal opened. Initial data:", initialData);
+            if (initialData) {
+                setValue('seatMapName', initialData.seatMapName || '');
+                setValue('floor', initialData.floor || 1);
+                setValue('row', initialData.row || 1);
+                setValue('column', initialData.column || 1);
+
+                setSelectedFloor(initialData.floor || 1);
+                setSelectedRow(initialData.row || 1);
+                setSelectedColumn(initialData.column || 1);
+
+                setSeats(initialData.seats || []);
+
+            } else {
+                reset();
+                setSeats([]);
+            }
+        }
+    }, [open, initialData, setValue, reset]);
+    const handleClose = () => {
+        reset();
+        onClose();
+    };
+    const onSubmitForm = (data: SeatMapData) => {
+        const completeData = {
+            ...data,
+            seats: seats
+        };
+        console.log("Submitting form:", completeData);
+        onSubmit(completeData, initialData ?? undefined);
+        handleClose();
+    };
+    
+
+    const renderSeatMap = () => {
+        const seatMaps = [];
+
+        for (let f = 0; f < floor; f++) {
+            const floorSeats = [];
+            for (let i = 0; i < row; i++) {
+                const rowSeats = [];
+                for (let j = 0; j < column; j++) {
+                    const seatKey = `Tầng ${f + 1} Ghế ${i + 1}-${j + 1}`;
+
+                    const existingSeat = seats.find(seat =>
+                        seat.floor === f + 1 && seat.row === i + 1 && seat.column === j + 1
+                    );
+
+                    rowSeats.push(
+                        <div key={`${seatKey}`} className={style.seat_card}>
+                            <input
+                                type="text"
+                                className="font-rounded font-semibold"
+                                placeholder="Tên ghế"
+                                required
+                                value={existingSeat ? existingSeat.name : ''}
+                                onChange={(e) => {
+                                    const seatData: SeatData = {
+                                        floor: f + 1,
+                                        row: i + 1,
+                                        column: j + 1,
+                                        name: e.target.value,
+                                        status: existingSeat ? existingSeat.status : false,
+                                    };
+
+                                    setSeats((prevSeats) => {
+                                        const updatedSeats = [...prevSeats];
+                                        const index = updatedSeats.findIndex(seat =>
+                                            seat.floor === seatData.floor &&
+                                            seat.row === seatData.row &&
+                                            seat.column === seatData.column
+                                        );
+                                        if (index > -1) {
+                                            updatedSeats[index] = { ...updatedSeats[index], name: seatData.name };
+                                        } else {
+                                            updatedSeats.push(seatData);
+                                        }
+                                        return updatedSeats;
+                                    });
+                                }}
+                            />
+
+                            <div className={style.toggle_container}>
+                                <span className={`font-rounded ${style.toggle_labe}`}>Kích hoạt</span>
+                                <label className={style.toggle_switch}>
+                                    <input
+                                        type="checkbox"
+                                        checked={existingSeat ? existingSeat.status : false}
+                                        onChange={(e) => {
+                                            console.log('Checked status:', e.target.checked);
+                                            const newStatus = e.target.checked;
+                                            const seatData: SeatData = {
+                                                floor: f + 1,
+                                                row: i + 1,
+                                                column: j + 1,
+                                                name: existingSeat ? existingSeat.name : '',
+                                                status: newStatus,
+                                            };
+
+                                            setSeats((prevSeats) => {
+                                                const updatedSeats = [...prevSeats];
+                                                const index = updatedSeats.findIndex(seat =>
+                                                    seat.floor === seatData.floor &&
+                                                    seat.row === seatData.row &&
+                                                    seat.column === seatData.column
+                                                );
+                                                if (index > -1) {
+                                                    updatedSeats[index] = { ...updatedSeats[index], status: newStatus };
+                                                } else {
+                                                    updatedSeats.push(seatData);
+                                                }
+                                                return updatedSeats;
+                                            });
+                                        }}
+                                        className={style.check}
+                                    />
+                                </label>
+                            </div>
+                        </div>
+                    );
+                }
+                floorSeats.push(
+                    <div key={`floor-${f}-row-${i}`} style={{ display: 'flex', justifyContent: 'center' }}>
+                        {rowSeats}
+                    </div>
+                );
+            }
+            seatMaps.push(
+                <div key={`floor-${f}`} style={{ margin: '20px 0' }}>
+                    <h4>{`Tầng ${f + 1}`}</h4>
+                    {floorSeats}
+                </div>
+            );
+        }
+        return seatMaps;
+    };
+
+    return (
+        <Modal open={open} onClose={handleClose}>
+            <Box sx={styleModal}>
+                <span className="text-[20px] text-blue-500">
+                    {initialData ? "Cập nhật thông tin sơ đồ ghế" : "Thêm sơ đồ ghế mới"}
+                </span>
+                <form onSubmit={handleSubmit(onSubmitForm)} className="mt-5">
+                    <Grid container spacing={4}>
+                        <Grid item xs={12} sm={6} md={6}>
+                            <label htmlFor="seatMapName" className="block font-rounded font-semibold text-black">Tên sơ đồ</label>
+
+                            <TextField
+                                fullWidth
+                                {...register('seatMapName', { required: 'Vui lòng nhập tên sơ đồ' })}
+                                error={!!errors.seatMapName}
+                                size="small"
+                                className="font-rounded"
+                                helperText={errors.seatMapName?.message}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={2}>
+                            <label htmlFor="floor" className="block font-rounded font-semibold text-black">Số tầng</label>
+                            <Select
+                                id="floor"
+                                {...register('floor')}
+                                className="w-full mt-1 border border-gray-300 rounded font-rounded"
+                                size="small"
+                                onChange={(e) => setSelectedFloor(Number(e.target.value))}
+                            >
+                                {Array.from({ length: 3 }, (_, index) => (
+                                    <MenuItem key={index + 1} value={index + 1} className="font-rounded">
+                                        {index + 1} tầng
+                                    </MenuItem>
+                                ))}
+
+                            </Select>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={2}>
+                            <label htmlFor="row" className="block font-rounded font-semibold text-black">Số hàng</label>
+                            <Select
+                                id="row"
+
+                                {...register('row')}
+                                className="w-full mt-1 border border-gray-300 rounded font-rounded"
+                                size="small"
+                                fullWidth
+                                onChange={(e) => setSelectedRow(Number(e.target.value))}
+                            >
+                                {Array.from({ length: 20 }, (_, index) => (
+                                    <MenuItem key={index + 1} value={index + 1} className="font-rounded">
+                                        {index + 1} hàng
+                                    </MenuItem>
+                                ))}
+
+                            </Select>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={2}>
+                            <label htmlFor="column" className="block font-rounded font-semibold text-black">Số cột</label>
+                            <Select
+                                id="column"
+                                {...register('column')}
+                                className="w-full mt-1 border border-gray-300 rounded font-rounded"
+                                size="small"
+                                fullWidth
+                                onChange={(e) => setSelectedColumn(Number(e.target.value))}
+                            >
+                                {Array.from({ length: 10 }, (_, index) => (
+                                    <MenuItem key={index + 1} value={index + 1} className="font-rounded">
+                                        {index + 1} cột
+                                    </MenuItem>
+                                ))}
+
+                            </Select>
+                        </Grid>
+                    </Grid>
+                    <div className="w-full">
+                        {renderSeatMap()}
+                    </div>
+
+                    <div className="flex justify-end mt-4">
+                        <Button sx={{ marginRight: 2 }} onClick={handleClose} variant="outlined" color="secondary">
+                            Hủy
+                        </Button>
+                        <Button type="submit" variant="contained" color="primary">
+                            {initialData ? "Cập nhật" : "Thêm"}
+                        </Button>
+                    </div>
+                </form>
+            </Box>
+        </Modal>
+    )
+}
+export default ModalSeatMap;
