f76b5d03cc9251c9f6a1fa03854354bf97c9690f
tuanthanh2603 <tuanthanhktltk21@gmail.com>
2024-11-27 16:03:09 +0700

    fix: init

--- src/app/ticket-02/page.tsx
+++ src/app/ticket-02/page.tsx
@@ -2,7 +2,7 @@
 import Calendar from "@/modules/trip/components/Calendar";
 import SelectRoute from "@/modules/route/components/SelectRoute";
 import ListTrip from "@/modules/trip/components/ListTrip";
-import { useRef, useState } from "react";
+import { useRef } from "react";
 import useRoute from "@/modules/route/hook/useRoute";
 import { getStorage_CompanyId } from "@/lib/cookie";
 
@@ -10,19 +10,19 @@ export default function TicketPage() {
   const companyId = Number(getStorage_CompanyId());
   const { routesName } = useRoute(companyId);
   console.log("List Route:" + routesName);
-  const [selectedRoute, setSelectedRoute] = useState<{
-    id: number;
-    name: string;
-  } | null>(null);
-  const handleRouteSelect = (route: { id: number; routeName: string }) => {
-    setSelectedRoute(route);
-    console.log("Tuyến đường được chọn:", route);
-  };
-  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
-  const handleDateChange = (date: Date | null) => {
-    setSelectedDate(date);
-    console.log("Ngày được chọn:", date);
-  };
+  // const [selectedRoute, setSelectedRoute] = useState<{
+  //   id: number;
+  //   name: string;
+  // } | null>(null);
+  // const handleRouteSelect = (route: { id: number; routeName: string }) => {
+  //   setSelectedRoute(route);
+  //   console.log("Tuyến đường được chọn:", route);
+  // };
+  // const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  // const handleDateChange = (date: Date | null) => {
+  //   setSelectedDate(date);
+  //   console.log("Ngày được chọn:", date);
+  // };
   const calendarRef = useRef<{ setToday: () => void } | null>(null);
   const handleTodayClick = () => {
     if (calendarRef.current) {
@@ -42,15 +42,15 @@ export default function TicketPage() {
             Hôm nay
           </button>
         </div>
-        <div className="mx-4 rounded-md">
+        {/* <div className="mx-4 rounded-md">
           <Calendar ref={calendarRef} onDateChange={handleDateChange} />
-        </div>
-        <div>
+        </div> */}
+        {/* <div>
           <SelectRoute
             routesName={routesName}
             onSelectRoute={handleRouteSelect}
           />
-        </div>
+        </div> */}
         <div className="mx-4">
           <button
             className="rounded bg-blue-500 px-5 py-[4.7px] font-bold text-white hover:bg-blue-700"
